6887c6556fca6d8c0cbe92e1955d018b3c3fbc0e
daniel <jabodav.daniel@gmail.com>
2023-05-13 16:44:02 +0300

    Initial commit

---
+++ Client.cpp
@@ -0,0 +1,75 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<semaphore.h>
+#include<thread>
+#include<iostream>
+#include<fcntl.h>
+
+#define BUF_SIZE 1024
+#define SHM_KEY 0x4444
+#define COLS 2        // number of columns in matrix
+
+int ROWS;
+
+sem_t* clientSemaphore = sem_open("/my_semaphore1", 0);
+sem_t* serverSemaphore = sem_open("/my_semaphore2", 0);
+
+int shmid = shmget(SHM_KEY, BUF_SIZE, IPC_CREAT | 0666);
+    
+int* shmaddr = (int*) shmat(shmid, NULL, 0);
+
+void writeToMemory()
+{
+    sem_wait(serverSemaphore); // Wait for memory access
+
+    // int matrix[ROWS][COLS] = {{1, 6}, {3, 1}, {7, 2}, {4, 4}, {8, 5}};
+     
+    for (int row = 0; row < ROWS; row++) // write matrix data to shared memory segment
+    {
+        for (int col = 0; col < COLS; col++) 
+        {
+            int buf = 0;
+            while (true)
+            {
+                std::cout << "Enter element " << row << " " << col << ":";
+                std::cin >> buf;
+                if (buf > -128 && buf < 127) break;
+                std::cout << "ERROR: Invalid value" << "\n";
+                std::cin.clear();
+                std::cin.ignore(10000, '\n');
+            }
+
+            *(shmaddr + row * COLS + col) = buf;
+        }
+    }
+
+    sem_post(clientSemaphore); // Set client to 1 and allow server to read info
+    sem_post(serverSemaphore); // Unlock access to memory memory for client
+}
+
+int main(int argc, char const *argv[])
+{
+    while (true)
+	{
+		std::cout << "Enter number of points (3-9) : ";
+		std::cin >> ROWS;
+		if (ROWS > 2 && ROWS < 10) break;
+		std::cout << "ERROR: Invalid value" << "\n";
+		std::cin.clear();
+		std::cin.ignore(10000, '\n');
+	}
+
+    //TODO: Initialize end
+    *(shmaddr + BUF_SIZE) = ROWS;
+    
+    writeToMemory();
+
+    return 0;
+}
---
+++ Server.cpp
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+#include<string.h>
+#include<errno.h>
+#include<stdlib.h>
+#include<unistd.h>
+#include<string.h>
+#include<semaphore.h>
+#include<thread>
+#include<iostream>
+#include<fcntl.h>
+
+#define BUF_SIZE 1024
+#define SHM_KEY 0x4444
+#define COLS 2        // number of columns in matrix
+
+int ROWS;
+
+sem_t* clientSemaphore = sem_open("/my_semaphore1", O_CREAT, 0666, 0);
+sem_t* serverSemaphore = sem_open("/my_semaphore2", O_CREAT, 0666, 1);
+
+int shmid = shmget(SHM_KEY, BUF_SIZE, IPC_CREAT | 0666);
+    
+int* shmaddr = (int*) shmat(shmid, NULL, 0);
+
+void readFromMemory()
+{
+    sem_wait(clientSemaphore); // Wait for client
+    sem_wait(serverSemaphore); // Block access to memory for client
+
+    int resultMatrix[ROWS][COLS];
+
+    for (int row = 0; row < ROWS; row++) //Read from memory 
+    {
+        for (int col = 0; col < COLS; col++) 
+        {
+            resultMatrix[row][col] = *(shmaddr + row * COLS + col);
+            std::cout << resultMatrix[row][col] << "\n";
+        }
+    }
+
+    double area = 0;
+
+    for(int i = 0; i < ROWS; i++) 
+    {
+        int j = (i + 1) % ROWS;
+        area += resultMatrix[i][0] * resultMatrix[j][1] - resultMatrix[j][0] * resultMatrix[i][1];
+    }
+
+
+    area = abs(area)/2;
+    std::cout << "The area is: " << area << "\n";
+
+    sem_post(serverSemaphore); // Unlock access to memory memory for client
+}
+
+int main(int argc, char const *argv[])
+{
+    for (size_t i = 0; i < 10; i++)
+    {   ROWS = *(shmaddr + BUF_SIZE);
+        readFromMemory();
+    }   
+    
+    return 0;
+}
